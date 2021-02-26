8f9f3fb79cf75262f35cf466a092895b5bc50cb5
Allefinho <alleffonseca@hotmail.com>
2021-02-26 20:54:19 -0300

    feat(Widgets): Taxa de simbolos

--- src/main.py
+++ src/main.py
@@ -13,16 +13,16 @@ from matplotlib.figure import Figure
 janela_principal = Tk()
 iniciar = False
 frequencia_digital = 2
-frequencia_analogica = 1
+taxa_simbolo = 3
 minimo_tamanho_intervalo = 0
 maximo_tamanho_intervalo = 15
-analogico = True
+taxaSimbolo = True
 digital = True
 
 janela_principal.title("Gerador de Sinais")
 janela_principal.resizable(True, True)
 janela_principal.config(bg="white")
-janela_principal.geometry("980x700")
+janela_principal.geometry("980x600")
 
 f = Figure(figsize=(10, 7), dpi=80)
 graficos = f.subplots(4)
@@ -69,26 +69,6 @@ def funcIniciar():
         iniciarBTN["text"] = "Pausar"
 
 
-def analogico():
-    global analogico
-    if analogico:
-        analogico = False
-        analogicoBTN["text"] = "Mostar analógico"
-    else:
-        analogico = True
-        analogicoBTN["text"] = "Ocutar analógico"
-
-
-def digital():
-    global digital
-    if digital:
-        digital = False
-        digitalBTN["text"] = "Mostar digital"
-    else:
-        digital = True
-        digitalBTN["text"] = "Ocutar digital"
-
-
 def seno(intervalo):
     global freqAngular
 
@@ -98,7 +78,7 @@ def seno(intervalo):
 def gerarGrafico(i, eixo_x, eixo_y):
     global intervalo, iniciar
     global maximo_tamanho_intervalo, minimo_tamanho_intervalo
-    global frequencia_analogica, frequencia_digital
+    global taxa_simbolo, frequencia_digital
 
     if iniciar:
         intervalo = np.linspace(
@@ -130,8 +110,8 @@ def gerarGrafico(i, eixo_x, eixo_y):
         graficos[3].set_ylabel("Amplitude")
 
         # Sinal Analógico
-        sinal_analogico = np.sin(intervalo)
-        graficos[0].plot(intervalo, sinal_analogico, "c")
+        taxaSimbolo = np.sin(intervalo)
+        graficos[0].plot(intervalo, taxaSimbolo, "c")
 
         # Sinal Sequência de Bits
         sinal_senquencia_bits = signal.square(intervalo)
@@ -160,15 +140,15 @@ def gerarGrafico(i, eixo_x, eixo_y):
         minimo_tamanho_intervalo += 1
 
 
-def setFrequenciaAnalogica(event):
-    global frequencia_analogica
-    if float(inputFrequenciaAnalogica.get().replace(",", ".")) >= 0:
-        frequencia_analogica = float(inputFrequenciaAnalogica.get().replace(",", "."))
-        frequenciaAnalogicoInfoLabel["text"] = inputFrequenciaAnalogica.get()
+def setTaxaSimbolo(event):
+    global taxa_simbolo
+    if float(inputTaxaSimbolo.get().replace(",", ".")) >= 0:
+        taxa_simbolo = float(inputTaxaSimbolo.get().replace(",", "."))
+        taxa_simbolo_InfoLabel["text"] = inputTaxaSimbolo.get()
     else:
-        messagebox.showerror("Erro", "Frequência inválida")
+        messagebox.showerror("Erro", "Taxa de símbolos inválida")
 
-    inputFrequenciaAnalogica.delete(0, END)
+    inputTaxaSimbolo.delete(0, END)
 
 
 def setFrequenciaDigital(event):
@@ -189,39 +169,29 @@ ani = animation.FuncAnimation(
     interval=500,
 )
 
-
+# Botão iniciar
 iniciarBTN = Button(janela_principal, width=24, height=3, bg="#d3d3d3", text="Iniciar", command=funcIniciar)
-iniciarBTN.place(relx=0.87, rely=0.45, anchor=N)
-
-analogicoBTN = Button(
-    janela_principal, width=12, text="Ocutar analógico", bg="#00CED1", command=analogico
-)
-analogicoBTN.place(relx=0.92, rely=0.4, anchor=N)
-
-digitalBTN = Button(
-    janela_principal, width=12, text="Ocutar digital", bg="#C4302B", command=digital
-)
-digitalBTN.place(relx=0.82, rely=0.4, anchor=N)
+iniciarBTN.place(relx=0.87, rely=0.65, anchor=N)
 
 ######################################
-############  Analógico   ############
+########  Taxa de Símbolos   #########
 ######################################
-frequenciaAnalogicoFrame = LabelFrame(
+taxa_simbolo_Frame = LabelFrame(
     janela_principal,
-    text="Frequência Analógico",
+    text="Taxa de Símbolos",
     width=145,
     height=75,
     borderwidth=0,
 )
-frequenciaAnalogicoFrame.place(in_=janela_principal, relx=0.87, rely=0.2, anchor=CENTER)
-frequenciaAnalogicoInfoLabel = Label(
-    frequenciaAnalogicoFrame,
-    text=str(frequencia_analogica),
+taxa_simbolo_Frame.place(in_=janela_principal, relx=0.87, rely=0.2, anchor=CENTER)
+taxa_simbolo_InfoLabel = Label(
+    taxa_simbolo_Frame,
+    text=str(taxa_simbolo),
 )
-frequenciaAnalogicoInfoLabel.place(relx=0.5, rely=0.15, anchor=N)
-inputFrequenciaAnalogica = Entry(frequenciaAnalogicoFrame, width=12)
-inputFrequenciaAnalogica.place(relx=0.5, rely=0.55, anchor=N)
-inputFrequenciaAnalogica.bind("<Return>", setFrequenciaAnalogica)
+taxa_simbolo_InfoLabel.place(relx=0.5, rely=0.15, anchor=N)
+inputTaxaSimbolo = Entry(taxa_simbolo_Frame, width=12)
+inputTaxaSimbolo.place(relx=0.5, rely=0.55, anchor=N)
+inputTaxaSimbolo.bind("<Return>", setTaxaSimbolo)
 
 ######################################
 ############    Digital   ############
