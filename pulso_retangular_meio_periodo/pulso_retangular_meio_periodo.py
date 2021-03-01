66fbfd880410db2b78981b50b634a526cc0b0ed5
Orion-Hunter <silvasalesf@gmail.com>
2021-03-01 16:23:56 -0300

    refactor(Mudar parametros): Modificacao params

--- pulso_retangular_meio_periodo/pulso_retangular_meio_periodo.py
+++ pulso_retangular_meio_periodo/pulso_retangular_meio_periodo.py
@@ -2,11 +2,10 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import numpy as np
 
-def gerar_pulso_retangular_meio_periodo(taxa_de_amostragem: int, periodo_de_simbolo: int):
+def gerar_pulso_retangular_meio_periodo(taxa_de_simbolo: int):
     '''
       Gerar o pulso conformador do tipo retangular de meio período
     '''
-    taxa_de_simbolo = int(taxa_de_amostragem / periodo_de_simbolo) 
     amostras = []
    
     for r in range(0, int(taxa_de_simbolo/2)):
@@ -17,7 +16,6 @@ def gerar_pulso_retangular_meio_periodo(taxa_de_amostragem: int, periodo_de_simb
     
     return amostras
         
-    
     
 
 
